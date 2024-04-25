179bef1f5c2a665a199d617084f3eac1a17704f0
sakuraaah <niktro30@gmail.com>
2024-04-25 15:19:52 +0300

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_22_121123) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lists", force: :cascade do |t|
     t.string "description"
     t.boolean "completed", default: false
